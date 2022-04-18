e5db385e3ff69fd91782bfdfaee17f240380affe
Blessing <bigbadumhe@gmail.com>
2022-04-17 19:36:17 -0500

    work so far

---
+++ app.js
@@ -0,0 +1,14 @@
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(namex, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+
+
